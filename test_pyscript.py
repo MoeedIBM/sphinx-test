996152836d0b420fa625033586dfaf170da8917b
Moeed Chaudhry <moeedchaudhry@Moeeds-MBP.fios-router.home>
2024-04-03 13:41:41 -0400

    initial push

---
+++ test_pyscript.py
@@ -0,0 +1,24 @@
+import os
+
+def list_directories(path):
+    directories = []
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+        if os.path.isdir(full_path):
+            directories.append(item)
+    return directories
+
+
+
+def main():
+    desktop_path = os.path.expanduser("~/Desktop")
+    directories = list_directories(desktop_path)
+    if directories:
+        print("Directories on Desktop:")
+        for directory in directories:
+            print(directory)
+    else:
+        print("No directories found on Desktop.")
+
+if __name__ == "__main__":
+    main()
